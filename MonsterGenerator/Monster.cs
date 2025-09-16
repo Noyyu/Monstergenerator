d7bff61a9980d2afa0e685e4500640f5b2875973
Nikki <Nikki.Norberg@hotmail.com>
2025-09-16 14:40:22 +0200

    La till lite kommentarer för klarhet

--- MonsterGenerator/Monster.cs
+++ MonsterGenerator/Monster.cs
@@ -9,7 +9,7 @@ namespace MonsterGenerator
 {
     internal class Monster
     {
-        //Setter private för att jag inte vill att användarens ka kunna komma åt variablerna direkt
+        //Setter private för att jag inte vill att användaren ska kunna komma åt variablernas "rå-data" direkt
         private string name     = "unnamed";
         private int health      = -1;
         private int stregnth    = -1;
@@ -17,9 +17,12 @@ namespace MonsterGenerator
         private static string[] randomNames = { "Angry boy", "Lil man", "Monster", "Stitch", "Sir Marmelade", "Mister Mister" }; // Static efter som att alla monster kommer ha tillgång till random namn
 
 
-        //------------
-        // Konstruktor
-        //------------
+        //----------------------------------------------------------------------------------------------------------------------------------------
+        // Konstruktor-
+        //
+        // Detta är default-värden ett Monster objekt får när den skapas. 
+        // Jag har gjort den privat för att jag vill att användaren ska använda någon utav mina "Factory medoder" för att skapa ett Monster objekt
+        //----------------------------------------------------------------------------------------------------------------------------------------
 
         private Monster(string name, int health, int stregnth)
         {
@@ -28,15 +31,51 @@ namespace MonsterGenerator
             this.stregnth = stregnth;
         }
 
+        //------------------------------------------------------------------------------------------------------------
+        // Static "factory" Methods
+        // Dessa metoder skapar och retunerar ett monster. 
+        // Dessa är statiska metoder, vilket betyder att dem tillhör KLASSEN Monster.
+        // Det innebär att dem enbart kan bli kallade på direkt från variabeln Monster och inte en isntans av klassen.
+        // Exempel: Monster myMonster =  Monster.CreateRandomMonster();
+        //------------------------------------------------------------------------------------------------------------
+
+        //"Create" methods
+        public static Monster CreateRandomMonster()
+        {
+            int random = new Random().Next(0, randomNames.Length - 1);
+            string randomName = randomNames[random];
+
+            int randomHealth = new Random().Next(5, 21);    // 5 - 20
+            int randomStregnth = new Random().Next(1, 11);  // 1 - 10
+
+            Console.WriteLine("A monster named " + randomName + " with " + randomHealth + " health and " + randomStregnth + " stregnth entered the battle field");
+
+            return new Monster(randomName, randomHealth, randomStregnth); //Retunerar en monster INSTANS
+        }
+
+        public static Monster CreateSpecificMonster(string name, int health, int stregnth)
+        {
+            Console.WriteLine("A monster named " + name + " with " + health + " health and " + stregnth + " stregnth entered the battle field");
+            return new Monster(name, health, stregnth); //Retunerar en monster INSTANS
+        }
 
         //----------------
-        // Instans Methods
+        // Instansmetoder
+        //
+        // Dessa metoder retunerar data specefikt till objektet du frågar om. 
+        // Exempel: 
+        // Monster myMonster = Monster.CreateSpecificMonster("lil boy", 10, 10);
+        // Monster mySecondMonster = Monster.CreateSpecifikMonster("bug boy", 15, 15);
+        // int health = myMonster.getHealth();
+        // health är nu 10
+        // int health = mySecondMonster.getHealth();
+        // Samma health variabel är nu 15
         //----------------
 
         //"Get" methods
         public int GetHealth()
         {
-            return this.health;
+            return this.health; //"this" refererar egentligen bara till att den specefikt ska retunera DETTA objektets värden. Egentligen onödigt nu men bra practice
         }
         public int GetStregnth()
         {
@@ -51,7 +90,7 @@ namespace MonsterGenerator
         //"Set" Methods
         public void SetName(string name)
         {
-            this.name = name;
+            this.name = name; //"Hör är "this" lite viktigare då båda objektets variabel och den inmatade variabeln har samma namn. Man kan även ba namnge dom anorlunda men jag är van vid detta. "
         }
 
         public void SetHealth(int health)
@@ -66,9 +105,8 @@ namespace MonsterGenerator
 
         //"Action" methods
         public int Attack()
-        {
-            
-            Console.WriteLine(this.name + " attacked!");
+        { 
+            Console.WriteLine(this.name + " attacked!"); 
             return this.stregnth;
         }
         public void takeDamage(int damage)
@@ -76,31 +114,6 @@ namespace MonsterGenerator
             this.health -= damage;
             Console.WriteLine(this.name + " took " + damage + " damage!");
             Console.WriteLine(this.name + " now has " + this.health + " health");
-        }
-
-
-        //------------------------
-        // Static "fabric" Methods
-        //------------------------
-
-        //"Create" methods
-        public static Monster CreateRandomMonster()
-        {
-            int random = new Random().Next(0, randomNames.Length - 1);
-            string randomName = randomNames[random];
-
-            int randomHealth = new Random().Next(5, 21);    // 5 - 20
-            int randomStregnth = new Random().Next(1, 11);  // 1 - 10
-
-            Console.WriteLine("A monster named " + randomName + " with " + randomHealth + " health and " + randomStregnth + " stregnth entered the battle field");
-
-            return new Monster(randomName, randomHealth, randomStregnth); //Retunerar en monster instans
-        }
-
-        public static Monster CreateSpecificMonster(string name, int health, int stregnth)
-        {
-            Console.WriteLine("A monster named " + name + " with " + health + " health and " + stregnth + " stregnth entered the battle field");
-            return new Monster(name, health, stregnth); //Retunerar en monster instans
         }
     }
 }
--- MonsterGenerator/Program.cs
+++ MonsterGenerator/Program.cs
@@ -7,8 +7,8 @@
             Monster m1 = Monster.CreateRandomMonster();
             Monster m2 = Monster.CreateSpecificMonster("Big Boy", 100, 30);
 
-
-
+            // m2.takeDamage() tar in en int som subtraheras från objektets health. 
+            // Efter som att m1.Attack() retunerar en int baserad på objektets stregnth variabel kan jag stoppa in den direkt i m2.takeDamage
             m2.takeDamage(m1.Attack());
         }
     }
